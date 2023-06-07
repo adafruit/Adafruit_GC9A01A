a03514ae042e990bc8d16f51aad7e87dabd989b1
Phillip Burgess <paintyourdragon@dslextreme.com>
2023-06-07 09:54:45 -0700

    Update graphicstest example w/Seeed display pinout + comments

--- examples/graphicstest/graphicstest.ino
+++ examples/graphicstest/graphicstest.ino
@@ -2,10 +2,34 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_GC9A01A.h"
 
-#define TFT_DC 9
-#define TFT_CS 10
+// Define pins for display interface. You'll probably need to edit this for
+// your own needs:
 
-// Hardware SPI on Feather or other boards
+#if defined(ARDUINO_SEEED_XIAO_RP2040)
+
+// Pinout when using Seed Round Display for XIAO in combination with
+// Seeed XIAO RP2040. Other (non-RP2040) XIAO boards, any Adafruit Qt Py
+// boards, and other GC9A01A display breakouts will require different pins.
+#define TFT_CS D1 // Chip select
+#define TFT_DC D3 // Data/command
+#define TFT_BL D6 // Backlight control
+
+#else // ALL OTHER BOARDS - EDIT AS NEEDED
+
+// Other RP2040-based boards might not have "D" pin defines as shown above
+// and will use GPIO bit numbers. On non-RP2040 boards, you can usually use
+// pin numbers silkscreened on the board.
+#define TFT_DC  9
+#define TFT_CS 10
+// If display breakout has a backlight control pin, that can be defined here
+// as TFT_BL. On some breakouts it's not needed, backlight is always on.
+
+#endif
+
+// Display constructor for primary hardware SPI connection -- the specific
+// pins used for writing to the display are unique to each board and are not
+// negotiable. "Soft" SPI (using any pins) is an option but performance is
+// reduced; it's rarely used, see header file for syntax if needed.
 Adafruit_GC9A01A tft(TFT_CS, TFT_DC);
 
 void setup() {
@@ -13,6 +37,11 @@ void setup() {
   Serial.println("GC9A01A Test!");
 
   tft.begin();
+
+#if defined(TFT_BL)
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH); // Backlight on
+#endif // end TFT_BL
 
   Serial.println(F("Benchmark                Time (microseconds)"));
   delay(10);
